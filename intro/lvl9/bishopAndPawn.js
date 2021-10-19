c08f6954f82caf2ae8948372a0bfb4a367e031f8
DustinDust <dustindusk@gmail.com>
2021-10-19 11:41:30 +0700

    test

--- intro/lvl9/bishopAndPawn.js
+++ intro/lvl9/bishopAndPawn.js
@@ -1,6 +1,4 @@
 const bishopAndPawn = (bishop, pawn) => {
   const bishopPos = bishop.split('').map((value) => value.charCodeAt(0));
-  const pawnPos = pawn.split('').map(((value) => value.charCodeAt(0)));
+  const pawnPos = pawn.split('').map((value) => value.charCodeAt(0));
 };
-
-console.log(bishopAndPawn('a1', 'c3'));
